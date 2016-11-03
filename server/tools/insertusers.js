53f2d633ae860bded2b544ec6fedd41cf23e489b
laekov <laekov.h@gmail.com>
2016-11-03 10:42:54 +0800

    contestLayoutUI

--- server/tools/insertusers.js
+++ server/tools/insertusers.js
@@ -2,17 +2,22 @@ var mongoose = require('mongoose')
 mongoose.connect('mongodb://127.0.0.1/tuojdata')
 var user=require('../models/user').user
 
-user.update({
-	userid: 'root'
+var userList = [{
+	userid: 'root',
+	userpassword: 'root',
+	power: 1
 }, {
-    $set: {
-        userpassword: 'root',
-        power: 1
-    }
-}, {
-	upsert: true
-}, function(err) {
-	if (err) {
-		console.warn(err);
-	}
+	userid: 'player0',
+	userpassword: 'player',
+}];
+
+userList.forEach(function(userInfo) {
+    user.update({}, { $set: userInfo }, { upsert: true }, function(err) {
+        if (err) {
+            return console.warn(err);
+        }
+        console.log('done');
+    });
 });
+
+mongoose.disconnect();
