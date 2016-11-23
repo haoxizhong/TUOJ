6a845550fd7fea79db6849e831917516b5b0e5f9
jiaaoHo <rivenhe@foxmail.com>
2016-11-23 21:04:31 +0800

    sandboxPath

--- judger/utils/sandbox/ccsp/bin/configure.h
+++ judger/utils/sandbox/ccsp/bin/configure.h
@@ -3,17 +3,26 @@
 
 //#define DEFAULT_HOME_PATH "/home/judger"
 //#define DEFAULT_HOME_PATH "/home/toby/Workspace/tuoj/judger/utils/sandbox/ccsp"
+#ifdef NOMHY
+#define DEFAULT_HOME_PATH "/home/laekov/TUOJ/judger/utils/sandbox/ccsp"
+#define DEFAULT_WHITELIST_PATH "/home/laekov/TUOJ/judger/utils/sandbox/ccsp/bin/whitelist"
+#define HOME_PATH "."
+#define RUN_PATH HOME_PATH ""
+#define SHARED_PATH HOME_PATH ""
+#define TMP_PATH HOME_PATH ""
+#else
 #define DEFAULT_HOME_PATH "/home/mhy12345/tuoj/judger/utils/sandbox/ccsp"
 #define DEFAULT_WHITELIST_PATH "/home/mhy12345/tuoj/judger/utils/sandbox/ccsp/bin/whitelist"
 #define HOME_PATH "/home/judger"
 #define RUN_PATH HOME_PATH "/run"
 #define SHARED_PATH HOME_PATH "/shared"
 #define TMP_PATH HOME_PATH "/tmp"
+#endif
 #define MAXBUF 250
 
 #ifdef __x86_64__
 typedef unsigned long long int reg_val_t;
-#define REG_SYSCALL orig_rax
+#define REG_SYSCALL orig_rax 
 #define REG_RET rax
 #define REG_ARG0 rdi
 #define REG_ARG1 rsi
