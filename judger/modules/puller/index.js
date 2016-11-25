153c137a4e03dbe54e6522b5dad079e96b7695bb
jiaaoHo <rivenhe@foxmail.com>
2016-11-26 04:57:59 +0800

    updatePuller

--- judger/modules/puller/index.js
+++ judger/modules/puller/index.js
@@ -1,4 +1,5 @@
 var request = require('request');
+var cp = require('child_process');
 var path = require('path');
 var Step = require('step');
 var fs = require('fs-extra');
@@ -14,15 +15,19 @@ module.exports = function(cfg) {
 	self.updateProb = function(url, md5, callback) {
 		self.id = md5;
 		self.path = path.resolve(self.cfg.path, self.id);
+		self.zipPath = path.resolve(self.cfg.path, self.id + '.zip');
 		try {
 			fs.readdirSync(self.path);
 			fs.readFileSync(path.resolve(self.path, 'tus.json'));
             callback(false, self.path);
         } catch (err) {
             fs.emptyDirSync(self.path);
-            request(url).pipe(unzip.Extract({ 
-                path: self.path 
-            })).on('close', function() {
+            request(url).pipe(fs.createWriteStream(self.zipPath))
+            .on('close', function() {
+				cp.execFileSync('/usr/bin/unzip', [ 
+						self.zipPath,
+						'-d',
+						self.path ]);
                 callback(false, self.path);
             }).on('error', function(err) {
                 if (err) {
