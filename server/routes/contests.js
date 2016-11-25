21ec2558fab6c39eb3a8b973beef7f50e6d833ef
Chenyao2333 <louchenyao@gmail.com>
2016-11-26 02:41:50 +0800

    fix bug

--- server/routes/contests.js
+++ server/routes/contests.js
@@ -122,7 +122,7 @@ router.get('/:cid([0-9]+)/problems/:pid([0-9]+)',function(req,res,next){
 	var problemid=parseInt(req.params.pid)
     contest.findOne({_id: contestid}).populate("problems").exec(function (err, c) {
 		if (err) next(err)
-        if (x.get_status() == 'unstarted') return next(new Error('Contest is not in progress!'));
+        if (c.get_status() == 'unstarted') return next(new Error('Contest is not in progress!'));
 		if (!c || problemid < 0 || problemid > c.problems.length) next()
 
 		p = c.problems[problemid];
