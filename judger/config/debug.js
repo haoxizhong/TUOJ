5270b30da55f1d68e80f892d5861a17b5751f3ae
jiaaoHo <rivenhe@foxmail.com>
2016-11-25 23:25:02 +0800

    judgerConfig

--- judger/config/debug.js
+++ judger/config/debug.js
@@ -2,7 +2,7 @@
 var sample = require('./sample');
 //sample.singleRun = true;
 sample.local.gitter.noFetch = true;
-//sample.local.tus.clean = false;
+sample.local.tus.clean = false;
 // sample.local.sandbox.containerId = 'a0ff8197041e';
 // sample.wwwServer.fetchUrl = 'http://183.172.153.141:3333/api/judge/get_task/acm';
 // sample.wwwServer.uploadUrl = 'http://183.172.153.141:3333/api/judge/update_results';
